37ee4cc091f425413f715418c945f95a9fd3fc63
wreising <wreising@mac.com>
2022-03-21 17:53:45 -0700

    schema and seed work

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE employees (
 	first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  manager_id INT,
+  FOREIGN KEY(role_id) REFERENCES roles(id),
+  FOREIGN KEY(manager_id) REFERENCES employess(id)
 );
 
 CREATE TABLE departments (
@@ -19,5 +21,6 @@ CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(50) NOT NULL,
   salary INT NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY(department_id) REFERENCES departments(id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,46 +1,47 @@
 USE employees_db;
 
+-- Employees
 INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (01, 'William', 'Reising', 01, 01);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (01, 'William', 'Reising', 01,);
 VALUES (02, 'Eric', 'Reising', 03, 01);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES (03, 'Sally', 'Edwards', 04, 01);
+VALUES (04, 'Dave', 'Zepp', 02, 01);
+-- VALUES (05, 'Sam', 'Jones', 06, 04);
+-- VALUES (06, 'Karen', 'Ross', 04, 03);
+-- VALUES (07, 'Phil', 'Brown', 06, 04);
+-- VALUES (08, 'Warner', 'Oxborow', 06, 04);
+-- VALUES (09, 'Tammy', 'Hall', 02, 02);
+-- VALUES (10, 'Robert', 'Hill', 07, 01);
+-- VALUES (11, 'Ken', 'Salgado', 07, 10);
+-- VALUES (12, 'Dave', 'Okada', 07, 10);
+-- VALUES (13, 'Sandra', 'Lee', 08, 01);
+-- VALUES (14, 'Mike', 'wagner', 10, 13);
+-- VALUES (15, 'Ryan', 'Stenberg', 09, 13);
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (04, 'Dave', 'Zepp', 06, 01);
+-- Departments
+INSERT INTO deprtments (id, name)
+VALUES (01, 'Executive');
+VALUES (02, 'Admin');
+VALUES (03, 'Legal');
+VALUES (04, 'Finance');
+-- VALUES (05, 'Engineering');
+-- VALUES (06, 'IT');
+-- VALUES (07, 'R&D');
+-- VALUES (08, 'Manufacturing');
+-- VALUES (09, 'QA');
+-- VALUES (10, 'Shipping');
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (05, 'Sam', 'Jones', 06, 04);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (06, 'Karen', 'Ross', 04, 03);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (07, 'Phil', 'Brown', 06, 04);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (08, 'Warner', 'Oxborow', 06, 04);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (09, 'Tammy', 'Hall', 02, 02);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (10, 'Robert', 'Hill', 07, 01);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (11, 'Ken', 'Salgado', 07, 10);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (12, 'Dave', 'Okada', 07, 10);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (13, 'Sandra', 'Lee', 08, 01);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (14, 'Mike', 'wagner', 10, 13);
-
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (15, 'Ryan', 'Stenberg', 09, 13);
+-- Roles
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (01, 'CEO', 250000, 01);
+VALUES (02, 'Assistant', 60000, 02);
+VALUES (03, 'Attorney', 125000, 03);
+VALUES (04, 'CFO', 200000, 04);
+-- VALUES ('VP Ops', 180000, 08);
+-- VALUES ('IT Manager', 140000, 06);
+-- VALUES ('R&D Manager', 90000, 07);
+-- VALUES ('R&D Tech', 50000, 07);
+-- VALUES ('IT Tech' 75000, 06);
+-- VALUES ('Controller', 85000, 04);
+-- VALUES ('QA Tech', 65000, 09);
+-- VALUES ('Shipping Tech', 45000, 10);
